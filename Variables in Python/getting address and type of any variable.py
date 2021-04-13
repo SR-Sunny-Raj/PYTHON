02699cadc75af8fb606865822ccf1dae2dd9b946
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-04-14 03:30:59 +0530

    getting address and type of any variable

---
+++ Variables in Python/getting address and type of any variable.py
@@ -0,0 +1,10 @@
+a = 10
+print(id(a))     # to get address of variable
+b = a
+print(id(b))
+name = 'sunny'
+print(id(name))
+print(id(10))
+PI = 3.14           # to just represent that it's a constant
+print(PI)
+print(type(PI))          # to get the type of the variable
