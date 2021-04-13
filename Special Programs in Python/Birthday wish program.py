dd3b44681aaf0f1988077666daa6add3b700d809
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-04-14 03:37:29 +0530

    Birthday wish program

---
+++ Special Programs in Python/Birthday wish program.py
@@ -0,0 +1,48 @@
+from time import sleep
+
+
+def start():
+    name = "Sunny"
+    print("\t\ti i i")
+    sleep(1)
+    print("\t\ti i i")
+    sleep(1)
+    print("\t:---------------------:")
+    print("\t:   Happy    ++++     :")
+    sleep(1)
+    print("\t:   ++++     Birthday :")
+    sleep(1)
+    print(f"\t:  {name}   ++++      :")
+    print("\t/______________________\ ")
+    sleep(1)
+    print("\t:-:-:-:-:-:-:-:-:-:-:-:")
+    sleep(.25)
+    print("\t:-:-:-:-:-:-:-:-:-:-:-:")
+    sleep(.25)
+    print("\t:-:-:-:-:-:-:-:-:-:-:-:")
+    sleep(.25)
+    print("\t―――――――――――――――――――――――")
+    sleep(.25)
+    print("\t||  ||  ||  ||  ||  || ")
+    sleep(.25)
+    print("\t||  ||  ||  ||  ||  || ")
+    sleep(.25)
+    print("\t||  ||  ||  ||  ||  || ")
+    sleep(.25)
+    print("\t||  ||  ||  ||  ||  || ")
+    sleep(.25)
+    print("\t||  ||  ||  ||  ||  || ")
+    sleep(.5)
+    print("Loading......")
+    sleep(1)
+    print("A message to you.....")
+    sleep(2)
+    print("\t MAY GOD BLESS YOU!! :)")
+
+
+a = input(
+    "1.TYPE 'win'\n2.PRESS 'ENTER' KEY\n(ELSE TYPE 'QUIT' TO QUIT)\n\n:").lower()
+if (a == "win"):
+    start()
+else:
+    print("BYE!!")
