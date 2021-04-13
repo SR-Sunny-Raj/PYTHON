a55b39c94b0a4ba3f489a6dd8d68f6236bffcaf8
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-04-14 03:35:48 +0530

    Tuple and Set in python

---
+++ Tuple and Set in python/Tuple and Set in python.py
@@ -0,0 +1,18 @@
+# tuple
+tup = (11, 55, 22, 33, 44, 99, 77, 44, 44)
+print(tup)
+print(tup[0])
+print(tup[-2])
+print(tup.count(44))
+print(tup.index(44))
+
+# set
+s = {10, 50, 40, 20, 90, 70, 80}
+print(s.pop())
+print(s)
+s.add(11)
+print(s)
+s.remove(90)
+print(s)
+s.clear()
+print(s)
