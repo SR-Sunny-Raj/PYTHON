8a228a66962531ac9454d586e5e8046e95fb484f
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-04-14 03:29:09 +0530

    variables

---
+++ Variables in Python/variables.py
@@ -0,0 +1,17 @@
+x = 4
+y = 5
+print(x+y)
+print(+x)
+name = 'YouTube'
+print(name)
+print('my '+name)
+names = 'my '+name
+print(name[2:])
+print(names[3:])
+nums = [2, 4, 6, 9, 11]
+naam = ['sunny', 'raj']
+print(nums)
+print(nums+naam)
+myname = 'sunny'
+print(10*myname)
+print(2**5)
