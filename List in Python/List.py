6de7b3bce52ec3a1e9f785b6d945213e16090a6b
Sunny Raj <61446913+SR-Sunny-Raj@users.noreply.github.com>
2021-04-14 03:31:53 +0530

    List

---
+++ List in Python/List.py
@@ -0,0 +1,29 @@
+nums = [1, 5, 3, 66, 20, 47, 94, 86]
+print(nums)
+names = ['Sunny', 'Raj', 'python', 'java']
+print(names)
+print(names[0])
+print(names[1:])
+mix = [nums, names]
+print(mix)
+all = [6.5, 'sunny', 13]
+print(all)
+nums.append(11)
+print(nums)
+nums.insert(1, 55)
+print(nums)
+nums.reverse()
+print(nums)
+nums.remove(47)
+print(nums)
+nums.pop(7)
+print(nums)
+nums.extend([33, 44, 55, 66])
+print(nums)
+nums.sort()
+print(nums)
+del nums[0:4]
+print(nums)
+print(min(nums))
+print(max(nums))
+print(sum(nums))
