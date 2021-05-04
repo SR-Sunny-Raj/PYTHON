102bdb3abb79084728abcb89dc998a1dcb5f1447
Sunny Raj <SRAJ1333.SR@GMAIL.COM>
2021-05-04 09:43:07 +0530

    Printing pattern

---
+++ Special Programs in Python/Printing pattern.py
@@ -0,0 +1,9 @@
+for i in range(1, 5):
+    for j in range(i):
+        print('# ', end="")
+    print()
+# second pattern
+for k in range(5, 1, -1):
+    for l in range(k-1):
+        print('# ', end="")
+    print()
