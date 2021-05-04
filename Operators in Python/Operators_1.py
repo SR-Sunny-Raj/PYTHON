ccaf86c549af94c67297635807cddc2071fcf796
Sunny Raj <SRAJ1333.SR@GMAIL.COM>
2021-05-04 09:49:41 +0530

    Operators in Python

---
+++ Operators in Python/Operators_1.py
@@ -0,0 +1,24 @@
+# Arithmetic operators (+,-,*,/,%)
+print(4+5, 2-3, 5*6, 10/4, 10 % 4)
+
+# Assignment operator (=)
+x = 4
+print(x)
+
+# Relational operator(<,>,<=,>=,!=,==)
+print(4 > 5)
+print(4 != 8)
+
+# Logical operator(and,or,not)
+print(4 > 2 and 5 < 1)
+print(4 > 2 or 5 < 1)
+print(not True)
+
+# Unary operator
+x = -5
+print(x)
+
+y = True
+print(int(y))
+z = False
+print(float(z))
