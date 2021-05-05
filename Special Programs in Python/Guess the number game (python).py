9344b468a5ef18e2be1f57e1b8d16ef7109d217f
Sunny Raj <SRAJ1333.SR@GMAIL.COM>
2021-05-05 07:32:39 +0530

    Update Guess the number game (python).py

--- Special Programs in Python/Guess the number game (python).py
+++ Special Programs in Python/Guess the number game (python).py
@@ -1,16 +1,25 @@
-print("\n* * * * * * * * Guess a number between 1 to 1000 * * * * * * * * * * *")
-n = 395
+import random
+lb = 1
+ub = 500
 g = 9
+print("\n* * * * * * * * Guess a number between {} to {} in {} gusses * * * * * * * * * * *".format(lb, ub, g))
+n = random.randint(lb, ub)
+# n = 395
 i = 0
 while(i < g):
     i += 1
     num = int(input("\nEnter your guessed number -: "))
-    if num > n:
+    if num > ub or num < lb:
+        print("Please enter your guess in the specified range")
+        i -= 1
+    elif num > n:
         print("\nActual number is smaller than your guess\nNumber of guesses left -: ", g-i)
     elif num < n:
         print("\nActual number is greater than your guess\nNumber of guesses left -: ", g-i)
     else:
-        print("\nYou WON!!\nNumber of guesses used - : ", i)
+        print("\n* * * * * * * * You WON!! * * * * * * * *\nNumber of guesses used - : ", i)
+        i -= 1
         break
 if i >= g:
-    print("\nGame Over\nYou Lost!!\nBetter luck next time")
+    print(
+        "\nActual number was -: {}\n\n* * * * * * * * Game Over * * * * * * * *\n* * * * * * * * You Lost!! * * * * * * * *\n* * * * * Better luck next time * * * * * ".format(n))
