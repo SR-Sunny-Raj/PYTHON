cdf03900d87335da820178581a6cf0014b965870
Sunny Raj <SRAJ1333.SR@GMAIL.COM>
2021-05-05 07:24:06 +0530

    Comments_escape_sequences

---
+++ Comments in Python/Comments_escape_sequences.py
@@ -0,0 +1,13 @@
+# this is a single line comment
+"""
+This is
+a multi
+line
+comment
+"""
+print("su\nny")
+print("su\\nny")           # double slash is an escape character
+print(r"su\nny")           # we can also use raw strrings
+print("sunny", "raj")
+print("sunny", end=" ")
+print("raj")
