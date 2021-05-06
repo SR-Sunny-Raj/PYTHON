dc030c8a2b8aed3059550d95564924be84065a0c
Sunny Raj <SRAJ1333.SR@GMAIL.COM>
2021-05-06 07:13:49 +0530

    File_writing_and_Appending

---
+++ Special Programs in Python/File_writing_and_Appending.py
@@ -0,0 +1,10 @@
+# f = open("E:\\My Codes\\Python Codes\\April 2021\\30-04-2021\\file2.txt", "a")
+# Counts the number of letters in the string
+# count = f.write("Sunny Raj\n")
+# print(count)
+
+# To read and write both at the same time
+f = open("E:\\My Codes\\Python Codes\\April 2021\\30-04-2021\\file2.txt", "r+")
+print(f.read())
+f.write("Python")
+# print(f.read())
