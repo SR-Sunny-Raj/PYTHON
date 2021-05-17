22562e03fcc3cba91569598d84e90544d7a3e0e5
Sunny Raj <SRAJ1333.SR@GMAIL.COM>
2021-05-17 15:33:26 +0530

    py

---
+++ Exercise/pytrain.py
@@ -0,0 +1,31 @@
+# x = 17
+# y = 3
+# # z = y
+# # y = x
+# # x = z
+# # print("x = ", x, "y = ", y)
+# print("Subtracting y from x = ", x-y)
+# print("Multiplication = ", x*y)
+# print("division = ", x//y)
+
+# x = 7
+# y = 4
+# print("x to power y = ",x**y)
+# print("Modulo = ",x%y)
+
+# x = 24
+# y = 24
+# z = 12
+# if(x > y and x > z):
+#     print(x, " is greater")
+# elif y > x and y > z:
+#     print(y, " is greater")
+# else:
+#     print(z, " is greater")
+
+# i = 0
+# for i in range(0,5):
+#     print(i, end="\t")
+# s = "Sunny"
+# for i in s:
+#     print(i*3)
