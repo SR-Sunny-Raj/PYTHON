dd130c8479bbba99344774d2314c5a5f35bb4b4e
Sunny Raj <SRAJ1333.SR@GMAIL.COM>
2021-05-18 15:32:34 +0530

    python

--- Exercise/pytrain.py
+++ Exercise/pytrain.py
@@ -33,16 +33,62 @@
 # for i in range(10, 101, 10):
 #     print(f"10 * {b} = ", i)
 #     b += 1
-a = '246'
-s = ' Hello World'
-print(s[1:4])
-print(s*2)
-print(a.isdigit())
-print(a.isalnum())
-print(a.isalpha())
-print(s.upper())        # new
-print(s)
-print(s.strip())  # new
+# a = '246'
+# s = ' Namaste World'
+# print(s[1:4])
+# print(s*2)
+# print(a.isdigit())
+# print(a.isalnum())
+# print(a.isalpha())
+# print(s.upper())        # new
+# print(s)
+# print(s.strip())  # new
+# print(s.isspace())   # new
+# print(s.split(" "))
+# print(s[13:0:-1])
+# a = float(input("Enter 1st number -: "))
+# b = float(input("Enter 2nd number -: "))
+# c = a+b
+# print(c)
+# print(fr"Sum is\t {c}")
 
-print(s.isspace())   # new
-print(s.split(" "))
+# def eo(x):
+#     if x % 2 == 0:
+#         print(f"{x} is even")
+#     else:
+#         print(f"{x} is odd")
+
+
+# x = int(input("Enter no -: "))
+# eo(x)
+
+# def fact(x):
+#     b = 1
+#     for i in range(x, 1, -1):
+#         b = b*i
+#     return b
+
+
+# a = int(input("Enter your number -: "))
+# if a == 0:
+#     print(1)
+# else:
+#     print("Factorial is -: ", fact(a))
+
+# def calc(a, b, o):
+#     if o == '+':
+#         print(f"Sum of {a} and {b} is -: ", a+b)
+#     elif o == '-':
+#         print(f"Subtraction of {a} and {b} is -: ", a-b)
+#     elif o == '*':
+#         print(f"Multiplication of {a} and {b} is -: ", a*b)
+#     elif o == '/':
+#         print(f"Division of {a} and {b} is -: ", a//b)
+
+
+# o = input("Enter the operator -: ")
+# a = int(input("Enter 1st number -: "))
+# b = int(input("Enter 2nd number -: "))
+# calc(a, b, o)
+# a = input("Enter the expression -: ")
+# print(eval(a))
